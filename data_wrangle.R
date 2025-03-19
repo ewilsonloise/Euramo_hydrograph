5b7a456021b4dfe3e3e63b81b00025117ec25b4a
Eloise Wilson <Eloise.Wilson@des.qld.gov.au>
2025-03-19 14:45:34 +1000

    data upload

---
+++ data_wrangle.R
@@ -0,0 +1,28 @@
+#Data Wrangle 
+
+#Libraries
+library(tidyverse)
+library(lubridate)
+
+#Step 1: Read in Height Data 
+Height_113004A<- read_csv("Data/113004A.H.CSV")
+Height_113006A<- read_csv("Data/113006A.H.CSV")
+Height_113015A<- read_csv("Data/113015A.H.CSV")
+
+#Step 1: Read in Rainfall Data
+Rainfall_114001A <- read_csv("Data/114001A.R.CSV")
+Rainfall_113015A <- read_csv("Data/113015A.R.CSV")
+Rainfall_113006A <- read_csv("Data/113006A.R.CSV")
+Rainfall_114001A <- read_csv("Data/")
+
+
+#Step 1: Read in Discharge Data
+Discharge_113015A <- read_csv("Data/113015A.Q.CSV")
+Discharge_113006A <- read_csv("Data/113006A.Q.CSV")
+Discharge_113004A<- read_csv("Data/113004A.Q.CSV")
+
+
+
+
+
+
