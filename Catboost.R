3f7970b97da848f8c4a612fa3be7b94ab6572b8b
Eloise Wilson <Eloise.Wilson@des.qld.gov.au>
2025-03-26 19:55:22 +1000

    stuff

--- Catboost.R
+++ Catboost.R
@@ -1,35 +1,13 @@
-# Introduction to CatBoost Algorithm
 
-#snippet from: Enhancing Flood Management Through Machine Learning: A Comprehensive Analysis of the CatBoost Application
-# https://doi.org/10.38124/ijisrt/IJISRT24JUN1770
+#26/03/2025
 
-# CatBoost (Categorical Boosting) is a Gradient Boosting Decision Trees (GBDT) algorithm 
-# designed to handle categorical features efficiently during training (Dorogush et al., 2018). 
-# It is widely used for prediction, recommendation, and ranking tasks (Peretz, 2018). 
+library(devtools)
+library(remotes)
+library(renv)
 
-# Unlike traditional GBDT methods, CatBoost processes categorical features dynamically 
-# during training rather than preprocessing, reducing data leakage. It uses a random 
-# permutation approach to encode categories, calculating the average label value for 
-# previous occurrences in the permutation (Xu et al., 2023). 
+# devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
 
-# CatBoost supports feature combinations, merging categorical features iteratively to 
-# improve splits. The algorithm applies a greedy approach to optimize feature interactions 
-# (Zhong et al., 2023). Each split in the tree is treated as a new category with two values 
-# (Huang et al., 2019). 
-
-# CatBoost has two tree-building modes: Ordered and Plain. Ordered mode mitigates gradient 
-# bias, while Plain mode follows the standard GBDT approach with ordered target statistics 
-# (Prokhorenkova et al., 2018). 
-
-# While CatBoost performs well with default settings, tuning key parameters improves 
-# accuracy. Training can be computationally expensive, but multiple random permutations 
-# help enhance robustness.
-
-
-install.packages('devtools')
-devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
-
-install.packages('devtools')
+library(catboost)
 
 
 
--- data_wrangle.R
+++ data_wrangle.R
@@ -8,32 +8,60 @@ library(readr)
 
 # Read in all the data as you have done previously
 df1 <- rbind(
-  (read_csv("Data/113004A.H.CSV") %>%
+  (read_csv("Data/Raw/113004A.H.CSV") %>%
   mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113006A.H.CSV") %>%
+  (read_csv("Data/Raw/113006A.H.CSV") %>%
     mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113015A.H.CSV") %>%
+  (read_csv("Data/Raw/113015A.H.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/114001A.R.CSV") %>%
+  (read_csv("Data/Raw/114001A.R.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113015A.R.CSV") %>%
+  (read_csv("Data/Raw/113015A.R.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113006A.R.CSV") %>%
+  (read_csv("Data/Raw/113006A.R.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113015A.Q.CSV") %>%
+  (read_csv("Data/Raw/113015A.Q.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113006A.Q.CSV") %>%
+  (read_csv("Data/Raw/113006A.Q.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H"))),
-  (read_csv("Data/113004A.Q.CSV") %>%
+  (read_csv("Data/Raw/113004A.Q.CSV") %>%
      mutate(time = format(time, "%Y-%m-%d %H")))) 
 
-df2 <- df1 %>% 
+dat <- df1 %>% 
   pivot_wider(names_from = "varname", values_from = "value") %>% 
   janitor::clean_names()
 
+skimr::skim(df2)
+summarytools::dfSummary(df2)
 
 
-write_csv(df2, "Data/dat.csv")
+#write_csv(dat, "Data/dat.csv")
+
+df2 <- rbind(
+  (read_csv("Data/Raw/Koombooloomba_Dam_031083_1800_Data.csv") %>%
+     unite(col = "time", Year, Month, Day, sep = "-")),
+  (read_csv("Data/Raw/Tully_Sugar_Mill_032042_1800_Data.csv") %>%
+    unite(col = "time", Year, Month, Day, sep = "-"))) %>%
+  mutate(time = as.Date(time)) %>%
+  filter(time >= as.Date("2009-12-23") & time <= as.Date("2024-08-01")) %>%
+  rename(
+    rainfall_mm = `Rainfall amount (millimetres)`,
+    site = `Bureau of Meteorology station number`,
+    quality = Quality) %>%
+  select(-`Product code`, -`Period over which rainfall was measured (days)`)
+
+df2$discharge_cumecs <- NA
+df2$var <- NA
+df2$level_metres <- NA
+
+column_order <- c("site", "time", "quality", "var", "rainfall_mm", "discharge_cumecs", "level_metres")
+
+dat <- dat %>% select(all_of(column_order))
+df2 <- df2 %>% select(all_of(column_order))
 
 
+(names(dat))
+(names(df2))
 
+dat_joined <- rbind(dat, df2)
+colnames(dat_joined)
